a67c411326d104c5a9a9d8c8de20e324021b47a2
daneg89 <daneg89@gmail.com>
2014-05-17 10:18:23 -0600

    Setting up program structure

---
+++ arg_handler.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+#
+# Takes the flags parsed from the command line and converts them to data
+# that can be assigned to our dictionary
+#
+# Params:
+#   flags - dictionary of flags
+#
+def set_data(flags):
+  pass
+
+def parse
