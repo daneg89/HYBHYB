842f5544535423c59bfb928b1d04bdb61cfe9ab2
daneg89 <daneg89@gmail.com>
2014-05-17 09:00:48 -0600

    More python scripts

---
+++ bpcs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+#
+# Performs embedding of a cover object using Bit-Plane Complexity Segmentation
+#
+# Params:
+#   file_path - String that indicates path of cover object
+#   data - Collection of bits that will be embedded
+#
+def bpcs_embed(file_path, data):
+  print "bpcs"
+
---
+++ constants.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+# Embedding IDs
+LSB = 1
+LSB_PR = 2
+BPCS = 3
+
+# Detection IDs
+HIST_ATTACK = 1
+VISUAL_ATTACK = 2
+FVISUAL_ATTACK = 3
