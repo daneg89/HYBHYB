bac43f00920acde75c77b4fd8f75129f9ae576bd
daneg89 <daneg89@gmail.com>
2014-05-22 23:48:28 -0600

    Removed pyc file, added the decode file mentioned in last commit

---
+++ decode.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+def decode_data(data):
+   """ Generic decoding function that determines how to decode a file
+
+   Params:
+      data - Dictionary that carries the following information
+         > "cover_obj": String
+         > "target_obj": String
+         > "key": String
+         > "method": Int
+         > "stats_mode": Bool
+         > "show_image": Bool
+         > "message": String
+         > "garbage": Bool
+   """
+   # TODO: Get the file to decode
+
+
+   # TODO: Calculate header length
+
+
+   # TODO: Perform the actual decoding
+
+   pass
+   
